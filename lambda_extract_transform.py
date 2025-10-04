9e494b3510b56455d0ed93fc866ba6930d4632d6
Mohammed Kausar <mohammedkawuser20@gmail.com>
2025-10-04 17:19:20 +0530

    split the extract transform and load actions, created separate deploy files

--- lambda_extract_transform.py
+++ lambda_extract_transform.py
@@ -6,10 +6,12 @@ import pandas as pd
 from src.ETL.extract import ExtractCities
 from src.ETL.transform import TransformCities
 from src.ETL.stage import StageCities
-from src.ETL.load import LoadCities
-
 
 pd.set_option('display.width', None)
+
+base = os.path.dirname(os.path.abspath(__file__))
+file = json.load(open(os.path.join(base, "config", "config.json")))
+
 def run_pipeline(config):
     print("Extraction started")
     extracted_data =ExtractCities(config).extract_data()
@@ -19,18 +21,16 @@ def run_pipeline(config):
     print("Staging the optimised type data in S3")
     cdc = max(transformed['dt'])
     StageCities(config,"complete",cdc).upload_file_to_s3(transformed)
-    print("Staging the optimised type data in DB raw table")
-    LoadCities(config).load_to_raw_staging(cdc)
-if __name__ == "__main__":
-    base = os.path.dirname(os.path.abspath(__file__))
-    file = json.load(open(os.path.join(base, "config", "config.json")))
-    run_pipeline(file)
+
+    #Update the max date value of that particular fetch in JSON
+    file["CDC"] =cdc
+    with open(os.path.join(base, "config", "config.json"),"w") as f:
+        json.dump(file,f,indent=4)
+    print(config["CDC"])
 
 def lambda_handler(event, context):
     try:
-        base_for_lambda = os.path.dirname(os.path.abspath(__file__))
-        file_for_lambda = json.load(open(os.path.join(base_for_lambda, "config", "config.json")))
-        run_pipeline(file_for_lambda)
+        run_pipeline(file)
         return {"status": "success"}
     except Exception as e:
         print(f"Error while invoking lambda: {str(e)}")
